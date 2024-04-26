0448dc5b7afab87e1fc9d3283ffe3dce9a2f2cab
Obakeng <60041842+obakeng-develops@users.noreply.github.com>
2024-04-26 20:21:32 +0200

    Add business logic for items controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,16 +1,56 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:show, :update, :delete]
+
   def index
+    @items = Item.all
+
+    render json: @items
+  end
+
+  def user_items
+    @items = @user.items
+
+    render json: @items
   end
 
   def create
+    @item = Item.new(item_params)
+
+    if @item.save
+      render json: @item, status: :created
+    else
+      render json: @item, status: :unprocessable_entity
+    end
   end
 
   def show
+    render json: @item
   end
 
   def update
+    if @item.update(item)
+      render json: @item, status: :ok
+    else
+      render json: @item, status: :unprocessable_entity
+    end
   end
 
   def delete
+    @item.destroy
+  end
+
+  private
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def set_user
+    @user = User.find(params[:user_id])
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: 'User not found' }, status: :not_found
+  end
+
+  def item_params
+    params.require(:item).permit(:name, :price, :type, :category_id, :user_id)
   end
 end
