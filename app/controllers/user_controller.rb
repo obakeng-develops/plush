ed1ea010b373fe2948eb98952d6bdd01ac9bb146
Obakeng <60041842+obakeng-develops@users.noreply.github.com>
2024-04-26 20:38:24 +0200

    Add business logic for user controller

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,13 +1,38 @@
 class UserController < ApplicationController
+  before_action :set_user, only: [:update, :delete]
+
   def show
+    render json: @item
   end
 
   def create
+    @user = Item.new(user_params)
+
+    if @user.save
+      render json: @user, status: :created
+    else
+      render json: @user, status: unprocessable_entity
+    end
   end
 
   def update
+    if @user.update(user_params)
+      render json: @user, status: :ok
+    else
+      render json: @user, status: :unprocessable_entity
+    end
   end
 
   def delete
+    @user.destroy
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password_digest)
   end
 end
