b08a8e24df161085c563991ef0cc8a1b85b4203d
Obakeng <60041842+obakeng-develops@users.noreply.github.com>
2024-04-26 19:36:06 +0200

    Add business logic for categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,13 +1,40 @@
 class CategoriesController < ApplicationController
+  before_action :set_category, only: [:update, :delete]
+
   def index
+    @categories = Category.all
+
+    render json: @categories
   end
 
   def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      render json: @category, status: :created
+    else
+      render json: @category.errors, status: :unprocessable_entity
+    end
   end
 
   def update
+    if @category.update(category_params)
+      render json: @category, status: :ok
+    else
+      render json: @category.errors, status: :unprocessable_entity
+    end
   end
 
   def delete
+    @category.destroy
+  end
+
+  private
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
