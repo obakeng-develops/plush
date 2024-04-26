f622fcf6d3807677b5c58ef0aa465b8922897d0f
Obakeng <60041842+obakeng-develops@users.noreply.github.com>
2024-04-26 19:37:11 +0200

    Add resource routes for categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,6 @@
 Rails.application.routes.draw do
-  get 'user/show'
-  get 'user/create'
-  get 'user/update'
-  get 'user/delete'
-  get 'categories/index'
-  get 'categories/create'
-  get 'categories/update'
-  get 'categories/delete'
-  get 'items/index'
-  get 'items/create'
-  get 'items/show'
-  get 'items/update'
-  get 'items/delete'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :categories, only: [:index, :create, :update, :delete]
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
