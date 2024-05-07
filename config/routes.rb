fe4ea872f410fdb4e0fb5fdb512ca4be136513ae
Obakeng <60041842+obakeng-develops@users.noreply.github.com>
2024-05-07 21:39:45 +0200

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,13 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :categories, only: [:index, :create, :update, :delete]
   resources :items, only: [:index, :create, :show, :update, :delete]
-  resources :user, only: [:show, :create, :update, :delete]
+  resources :user, only: [:show, :create, :update, :delete] do
+    get 'items', to: 'items#user_items'
+  end
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-  get '/items/u/:user_id', to: 'items#user_items'
 
   # Defines the root path route ("/")
   # root "posts#index"
